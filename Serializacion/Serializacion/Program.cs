f187b7f350ae0b4b387110b3622176c726896246
Lemito66 <lemito66@gmail.com>
2024-04-29 10:47:11 -0500

    Serialización en c#

---
+++ Serializacion/Serializacion/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+
+// función lambda
+
+var people = new People()
+{
+    Name = "Emill",
+    Age = 26
+};
+
+string json = JsonSerializer.Serialize(people);
+Console.WriteLine(json);
+
+public class People()
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public static string Get() => "Hello World!";
+}
+
+
+
