f9e3d412ec65c7711e490fc40c336d652d7c1cb1
Lemito66 <lemito66@gmail.com>
2024-04-29 10:47:27 -0500

    opción de uso de LINQ en c#

---
+++ Linq/Linq/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Data source.
+int[] scores = [90, 71, 82, 93, 75, 82];
+
+// Query Expression.
+IEnumerable<int> scoreQuery = //query variable
+    from score in scores //required
+    where score > 80 // optional
+    orderby score descending // optional
+    select score; //must end with select or group
+
+// Execute the query to produce the results
+foreach (var testScore in scoreQuery)
+{
+    Console.WriteLine(testScore);
+}
+
+// Output: 93 90 82 82
